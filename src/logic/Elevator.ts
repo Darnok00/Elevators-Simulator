04cb99647cf0e306e01579413c5ee62d7da486f1
Konrad Krzeminski <majunia28@gmail.com>
2023-05-02 23:19:49 +0200

    feat: add basic functions for Elevator class

--- src/logic/Elevator.ts
+++ src/logic/Elevator.ts
@@ -1,18 +1,37 @@
 import { ElevatorRide } from "../types/types";
 
 class Elevator {
+    private totalFloorsNumber: Number;
     private actualFloorNumber: Number;
     private destinationFloor: Number;
     private stops: Array<Number>;
     private elevatorRidesQueue: Array<ElevatorRide>;
 
 
-    constructor() {
+    constructor(totalFloorsNumber: Number) {
+        this.totalFloorsNumber = totalFloorsNumber;
         this.actualFloorNumber = 0;
         this.destinationFloor = 0;
         this.stops = []
         this.elevatorRidesQueue = []
     };
+
+    public getActualFloorNumber ()  {
+        return this.actualFloorNumber
+    }
+
+    public getDestinationFloor () {
+        return this.destinationFloor
+    }
+
+    public isGoing () {
+        return this.actualFloorNumber != this.destinationFloor
+    }
+
+    public hasPlannedRide () {
+        return this.elevatorRidesQueue.length > 0
+    }
+
 
 }
 
